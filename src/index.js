e45cc0f306a0a2ae807d8dbbca912a74d0331d91
devjinx <t.chareonlertkamol@gmail.com>
2024-05-13 13:25:28 +0700

    update routes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Wrap your render code inside a function for better organization and testing
+const renderApp = () => {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+};
+
+// Call the render function once to initially render the app
+renderApp();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// Optionally, you can export the renderApp function if you want to reuse it in tests
+export default renderApp;
