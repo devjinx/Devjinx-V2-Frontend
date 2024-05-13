392421abf4492ebd51243a1699b00b892080e1d8
devjinx <t.chareonlertkamol@gmail.com>
2024-05-13 13:34:36 +0700

    update logo and title'

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// Wrap your render code inside a function for better organization and testing
 const renderApp = () => {
   const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
@@ -13,13 +12,6 @@ const renderApp = () => {
   );
 };
 
-// Call the render function once to initially render the app
 renderApp();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-// Optionally, you can export the renderApp function if you want to reuse it in tests
-export default renderApp;
